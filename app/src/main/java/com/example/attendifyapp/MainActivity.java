939ca9df9e92e191329a6287825b05b411b1a63a
TOSHIBA <Sohefab@gmail.com>
2024-10-07 22:00:21 +0600

    first commit

---
+++ app/src/main/java/com/example/attendifyapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.attendifyapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
